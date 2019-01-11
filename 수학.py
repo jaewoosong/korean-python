733d8ab02fefe8e90235ba6c24c619a8f1f2d99f
Jaewoo Song <jsong.cse@gmail.com>
2019-01-11 16:27:42 +0800

    절댓값, 팩토리얼 추가

--- 수학.py
+++ 수학.py
@@ -1,13 +1,25 @@
 def 천장(x):
-    u"""천장 함수 구현
-
-    2019/01/01: 버전 1.0
-    """
+    u"""천장 함수"""
     if isinstance(x, int):
         return x
     else:
         결과 = int(x) # int()는 0에 가까워지는 쪽으로 소수점 버림
-        if 결과 > 0:
-            결과 += 1
+        return 결과 if 결과 <= 0 else 결과 + 1
+
+def 절댓값(x):
+    u"""절댓값 함수"""
+    return x if x >= 0 else -x
+
+def 팩토리얼(x):
+    u"""팩토리얼 함수"""
+    if x < 0:
+        raise ValueError()
+    elif x == 0:
+        return 1
+    else:
+        결과 = 1
+        while x > 1:
+            결과 *= x
+            x -= 1
         return 결과
 
--- 수학시험.py
+++ 수학시험.py
@@ -12,6 +12,25 @@ class 수학시험(unittest.TestCase):
             값2 = 수학.천장(x)
             self.assertEqual(값1, 값2)
 
+    def test_절댓값(self):
+        print('절댓값 함수 시험')
+        입력 = [3.7, 2, 0, -4, -5.9]
+        for x in 입력:
+            값1 = math.fabs(x)
+            값2 = 수학.절댓값(x)
+            self.assertEqual(값1, 값2)
+
+    def test_팩토리얼(self):
+        print('팩토리얼 함수 시험')
+        입력1 = [10, 5, 1, 0]
+        입력2 = [-2, 3.7 -5.9]
+        for x in 입력1:
+            값1 = math.factorial(x)
+            값2 = 수학.팩토리얼(x)
+            self.assertEqual(값1, 값2)
+        for x in 입력2:
+            self.assertRaises(ValueError, lambda x: 수학.팩토리얼(x), x)
+
 if __name__ == '__main__':
     unittest.main()
 
