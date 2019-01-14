2cb9094945e61b514c06d60a18ceb79633bf09e0
jaewoo <jsong.st@gmail.com>
2019-01-14 18:11:13 +0800

    최대공약수, 사인함수 (assertAlmostEqual 첫 사용)

--- 수학.py
+++ 수학.py
@@ -1,5 +1,10 @@
 # -*- coding: UTF-8 -*-
 
+# 상수값 얻어온 곳
+# https://github.com/python/cpython/blob/master/Modules/mathmodule.c
+
+파이 = 3.141592653589793238462643383279502884197
+
 def 바닥(x):
     u"""바닥 함수"""
     if isinstance(x, int):
@@ -12,6 +17,23 @@ def 절댓값(x):
     u"""절댓값 함수"""
     return x if x >= 0 else -x
 
+def 사인(x):
+    u"""사인 함수 (멱급수 사용)"""
+    x = x % (2*파이) # 계산 단순화
+    def 일반항(n):
+        분자 = ((-1)**n) * (x**(2*n+1))
+        분모 = 팩토리얼(2*n+1)
+        return 분자/분모
+    n = 0
+    현재항 = 일반항(n)
+    총합 = 0
+    오차범위 = 1.0e-6
+    while(절댓값(현재항) > 오차범위):
+        총합 += 현재항
+        n += 1
+        현재항 = 일반항(n)
+    return 총합
+
 def 천장(x):
     u"""천장 함수"""
     if isinstance(x, int):
@@ -19,6 +41,17 @@ def 천장(x):
     else:
         결과 = int(x) # int()는 0에 가까워지는 쪽으로 소수점 버림
         return 결과 if 결과 <= 0 else 결과 + 1
+
+def 최대공약수(a, b):
+    u"""최대공약수 함수: 유클리드 호제법 사용"""
+    큰 = a if a >= 0 else -a
+    작은 = b if b >= 0 else -b
+    if 큰 < 작은:
+        큰, 작은 = 작은, 큰
+    if 작은 == 0:
+        return 큰
+    나머지 = 큰 % 작은
+    return 작은 if 나머지 == 0 else 최대공약수(나머지, 작은)
 
 def 팩토리얼(x):
     u"""팩토리얼 함수"""
--- 수학시험.py
+++ 수학시험.py
@@ -20,11 +20,25 @@ class 수학시험(unittest.TestCase):
             값2 = 수학.절댓값(x)
             self.assertEqual(값1, 값2)
 
+    def test_사인(self):
+        입력 = [0, 1.2, 0.5*수학.파이, 4, 5.78, 2*수학.파이, 2*수학.파이, 1002]
+        for x in 입력:
+            값1 = math.sin(x)
+            값2 = 수학.사인(x)
+            self.assertAlmostEqual(값1, 값2, places=5)
+
     def test_천장(self):
         입력 = [3.7, 2, 0, -4, -5.9]
         for x in 입력:
             값1 = math.ceil(x)
             값2 = 수학.천장(x)
+            self.assertEqual(값1, 값2)
+
+    def test_최대공약수(self):
+        입력 = [(36, 24), (78696, 19332), (0, 756), (-3, 0), (-12, -294)]
+        for (ㄱ, ㄴ) in 입력:
+            값1 = math.gcd(ㄱ, ㄴ)
+            값2 = 수학.최대공약수(ㄱ, ㄴ)
             self.assertEqual(값1, 값2)
 
     def test_팩토리얼(self):
