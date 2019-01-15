69f49da39975d9fafa7d8a511efb213b67cbba96
jaewoo <jsong.st@gmail.com>
2019-01-15 15:25:48 +0800

    불필요한 print문 제거

--- 통계시험.py
+++ 통계시험.py
@@ -10,8 +10,6 @@ class 수학시험(unittest.TestCase):
         자료1 = [10, 5, 1, 0]
         자료2 = [1.7, 2.3, -5, 132, 0.2]
         자료3 = []
-        print(통계.평균(자료1))
-        print(통계.평균(자료2))
         self.assertEqual(통계.평균(자료1), statistics.mean(자료1))
         self.assertEqual(통계.평균(자료2), statistics.mean(자료2))
         self.assertRaises(statistics.StatisticsError, lambda x: 통계.평균(x), 자료3)
