e24dc065fa55f2ff8095c1306fd9f2a5a3934f56
Jaewoo Song <jaewoosong@users.noreply.github.com>
2019-01-30 11:58:27 +0800

    (1) 중앙값에 대한 시험 문제 추가 (2) 잘못된 클래스 명 수정

--- 통계시험.py
+++ 통계시험.py
@@ -4,7 +4,15 @@ import unittest
 import statistics
 import 통계
 
-class 수학시험(unittest.TestCase):
+class 통계시험(unittest.TestCase):
+
+    def test_중앙값(self):
+        자료1 = [10, 5, 1, 0]
+        자료2 = [1.7, 2.3, -5, 132, 0.2]
+        자료3 = []
+        self.assertEqual(통계.중앙값(자료1), statistics.median(자료1))
+        self.assertEqual(통계.중앙값(자료2), statistics.median(자료2))
+        self.assertRaises(statistics.StatisticsError, lambda x: 통계.중앙값(x), 자료3)
 
     def test_평균(self):
         자료1 = [10, 5, 1, 0]
